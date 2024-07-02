61ce29e16248bfc10cfb30491de35f5889a1c3a3
Boris2904 <95974028+Boris2904@users.noreply.github.com>
2024-07-02 16:28:21 +0300

    ValidateProfile

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -2,12 +2,50 @@
   <div>
     <h1>User Profile</h1>
     <p>Welcome to your profile page.</p>
+    <form @submit.prevent="register">
+      <label for="phone">Phone Number:</label>
+      <input type="tel" id="phone" v-model="phone" @blur="validatePhone" :class="{ error: isPhoneError }" required>
+      <div v-if="isPhoneError" class="error-message">{{ phoneError }}</div>
+      <button :disabled="!isFormValid" type="submit">Register</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Profile'
+  name: 'Registration',
+  data() {
+    return {
+      phone: '',
+      phoneError: '',
+      isPhoneError: false,
+    };
+  },
+  computed: {
+    isFormValid() {
+      return this.phone.trim() === '' &&
+          !this.isPhoneError;
+    }
+  },
+  methods: {
+    validatePhone() {
+      const phoneRegex = /(?:\+375|80)\s?\(?\d\d\)?\s?\d\d(?:\d[\-\s]\d\d[\-\s]\d\d|[\-\s]\d\d[\-\s]\d\d\d|\d{5})/;
+      if (this.phone.trim() === '') {
+        this.phoneError = 'Phone number is required';
+        this.isPhoneError = true;
+      } else if (!phoneRegex.test(this.phone.trim())) {
+        this.phoneError = 'Phone number is incorrect';
+        this.isPhoneError = true;
+      } else {
+        this.phoneError = '';
+        this.isPhoneError = false;
+      }
+    },
+    register() {
+      console.log('Registering user:', this.username, this.email, this.password, this.confirmPassword,this.phone);
+      this.$router.push({ name: 'profile' });
+    }
+  }
 };
 </script>
 
